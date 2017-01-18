fbf43a5bbcbebad039bb3c91185dbda8dfbc729e
Guardryan <Guard.ryan45@yahoo.com>
2017-01-18 00:21:32 -0500

    added comments and TODOs for future edits

--- src/Chess/ChessBoard.java
+++ src/Chess/ChessBoard.java
@@ -1,22 +1,36 @@
 package Chess;
 
 import Chess.Pieces.*;
-
 import java.util.ArrayList;
+
+/**
+ * Chess Board is a class for the board that is able to set up the starting positions of the pieces for a game of chess
+ * as well as return tuples that correspond to the values of the chess pieces locations including a specific method for
+ * the King's location.
+ */
 
 public class ChessBoard {
     private Tile[][] board;
 
+    /**
+     * Initilizes the chess board of a 8x8 2d Tile array
+     */
     public ChessBoard(){
         board = new Tile[8][8];
         initializeBoard();
         fillBoard();
     }
 
+    /**
+     * Returns the array of tiles that consists of the board.
+     */
     public Tile[][] getBoardArray(){
         return board;
     }
 
+    /**
+     * Designates which tiles of the board are black or white
+     */
     private void initializeBoard(){
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++) {
@@ -26,7 +40,12 @@ public class ChessBoard {
         }
     }
 
-    //Will break on boards with no Kings of 'color'. Should never happen.
+    // initial project person TODO: Will break on boards with no Kings of 'color'. Should never happen.
+    /**
+     * Retutrns the location of the King of a specific color.
+     * @param color
+     * @return a tuple containing the coordinates of the location
+     */
     public Tuple getKingLocation(ChessPiece.PieceColor color){
         Tuple location = new Tuple(-1,-1);
         for (int x = 0; x < board.length; x++){
@@ -42,6 +61,11 @@ public class ChessBoard {
         return location;
     }
 
+    /**
+     * returns an array of tuples containing the positions of the pieces of a color
+     * @param color
+     * @return location tuple
+     */
     public Tuple[] getAllPiecesLocationForColor(ChessPiece.PieceColor color){
         ArrayList<Tuple> locations = new ArrayList<>();
         for (int x = 0; x < board.length; x++){
@@ -53,43 +77,48 @@ public class ChessBoard {
         return locations.toArray(new Tuple[0]);//allocate new array automatically.
     }
 
+    /**
+     * translates a tuple to the corresponding location on the board
+     * @param tuple to be translated
+     * @return location of the board
+     */
     public Tile getTileFromTuple(Tuple tuple){
         return board[tuple.Y()][tuple.X()];
     }
 
-    /*
-    Initial filler of board
+    /**
+     * Populates the board with the chess pieces of a typical starting slate of a board.
      */
     private void fillBoard(){
-        //pawns
+        // Pawns
         for(int i = 0; i < 8; i++){
-        board[1][i].setPiece(new Pawn(ChessPiece.PieceColor.Black));
-        board[6][i].setPiece(new Pawn(ChessPiece.PieceColor.White));
+            board[1][i].setPiece(new Pawn(ChessPiece.PieceColor.Black));
+            board[6][i].setPiece(new Pawn(ChessPiece.PieceColor.White));
         }
 
-        //rooks
+        // Rooks
         board[0][0].setPiece(new Rook(ChessPiece.PieceColor.Black));
         board[0][7].setPiece(new Rook(ChessPiece.PieceColor.Black));
         board[7][0].setPiece(new Rook(ChessPiece.PieceColor.White));
         board[7][7].setPiece(new Rook(ChessPiece.PieceColor.White));
 
-        //knight
+        // Knight
         board[0][1].setPiece(new Knight(ChessPiece.PieceColor.Black));
         board[0][6].setPiece(new Knight(ChessPiece.PieceColor.Black));
         board[7][1].setPiece(new Knight(ChessPiece.PieceColor.White));
         board[7][6].setPiece(new Knight(ChessPiece.PieceColor.White));
 
-        //bishop
+        // Bishop
         board[0][2].setPiece(new Bishop(ChessPiece.PieceColor.Black));
         board[0][5].setPiece(new Bishop(ChessPiece.PieceColor.Black));
         board[7][2].setPiece(new Bishop(ChessPiece.PieceColor.White));
         board[7][5].setPiece(new Bishop(ChessPiece.PieceColor.White));
 
-        //queens
+        // Queens
         board[0][3].setPiece(new Queen(ChessPiece.PieceColor.Black));
         board[7][3].setPiece(new Queen(ChessPiece.PieceColor.White));
 
-        //kings
+        // Kings
         board[0][4].setPiece(new King(ChessPiece.PieceColor.Black));
         board[7][4].setPiece(new King(ChessPiece.PieceColor.White));
     }
--- src/Chess/ChessGame.java
+++ src/Chess/ChessGame.java
@@ -4,12 +4,18 @@ import Chess.ChessPiece.PieceColor;
 import Console.BoardDisplay;
 import java.util.ArrayList;
 
+/**
+ * Methods for general play of chess.
+ */
 public class ChessGame {
 
     private ChessBoard board;
     private boolean isFinished;
     private PieceColor currentPlayer;
 
+    /**
+     * Starts up the game with initial conditions and displays the board.
+     */
     public ChessGame(){
         board = new ChessBoard();
         currentPlayer = PieceColor.White;
@@ -19,6 +25,12 @@ public class ChessGame {
         BoardDisplay.printBoard(board);
     }
 
+    /**
+     * Takes the input of a piece to be moved from and to a position and moves the piece if it is a valid move.
+     * @param from current position of the game piece
+     * @param to future position of the game piece
+     */
+    // Ryan Guard TODO: change tuple to a more descriptive structure
     public void playMove(Tuple from, Tuple to){
         if(isValidMove(from, to)) {
             Tile fromTile = board.getBoardArray()[from.Y()][from.X()];
@@ -34,6 +46,11 @@ public class ChessGame {
             System.out.println("Invalid move!");
     }
 
+    /**
+     * Checks to see if the current player color is in the position of a check mate.
+     * @param color Color of the current player.
+     * @return Returns the check mate status.
+     */
     public boolean isColorCheckMate(PieceColor color){
         Tuple kingLocation = board.getKingLocation(color);
         ChessPiece king = board.getTileFromTuple(kingLocation).getPiece();
@@ -44,7 +61,7 @@ public class ChessGame {
             int newY = kingLocation.Y() + move.y;
             Tuple newLocation = new Tuple(newX, newY);
 
-            //TODO check if king can move here (eg. is own piece taking spot)
+            //(made by Original creator) TODO check if king can move here (eg. is own piece taking spot)
 
             if (!isLocationCheckForColor(newLocation, color)){
                 checkMate = false;
@@ -54,11 +71,24 @@ public class ChessGame {
         return checkMate;
     }
 
+    // Ryan Guard. TODO: remove pointless method below/confirm it is pointless
+    /**
+     * Checks that moves doesn't put oneself in check.
+     * @param kingColor
+     * @return
+     */
     public boolean isKingCheck(PieceColor kingColor){
         Tuple kingLocation = board.getKingLocation(kingColor);
         return isLocationCheckForColor(kingLocation, kingColor);
     }
 
+    /**
+     * checks if the given location contains the specified color
+     * @param location
+     * @param color
+     * @return returns true if not specified color and is valid and false if not
+     */
+    // TODO: rename function to isLocation
     private boolean isLocationCheckForColor(Tuple location, PieceColor color){
         PieceColor opponentColor = ChessPiece.opponent(color);
         Tuple[] piecesLocation = board.getAllPiecesLocationForColor(opponentColor);
@@ -72,11 +102,20 @@ public class ChessGame {
         return isCheck;
     }
 
+    /**
+     * Ends turn and switches player's colors for next turn's logic.
+     */
     private void endTurn(){
         if (currentPlayer == PieceColor.White) currentPlayer = PieceColor.Black;
         else currentPlayer = PieceColor.White;
     }
 
+    /**
+     * Tests to see if the move for the piece at the from coordinates can make a move to the "to" coordinates.
+     * @param from
+     * @param to
+     * @return returns true if the path is a legal move and false if it is not
+     */
     public boolean isValidMove(Tuple from, Tuple to){
         Tile fromTile = board.getTileFromTuple(from);
         Tile toTile = board.getTileFromTuple(to);
@@ -97,7 +136,7 @@ public class ChessGame {
                 fromTile.setPiece(fromPiece);//revert
                 return false;
             }
-            //if mate, finish game
+            // if move ends in check mate, finish game
             if (isColorCheckMate(ChessPiece.opponent(currentPlayer)))
                 isFinished = true;
 
@@ -108,6 +147,12 @@ public class ChessGame {
         return false;
     }
 
+    /**
+     * Tests if the move is valid for the given color
+     * @param takeColor
+     * @param locationToTake
+     * @return true if valid/false if not
+     */
     public boolean canColorTakeLocation(PieceColor takeColor, Tuple locationToTake){
         Tuple[] locations = board.getAllPiecesLocationForColor(takeColor);
         boolean canTake = false;
@@ -120,6 +165,12 @@ public class ChessGame {
         return canTake;
     }
 
+    /**
+     * calculates and returns an array of all possible moves for a given piece based off of its location
+     * @param piece
+     * @param currentLocation
+     * @return array of moves.
+     */
     private Move[] allPossibleMovesForPiece(ChessPiece piece, Tuple currentLocation){
         Move[] moves = piece.moves();
         ArrayList<Move> possibleMoves = new ArrayList<>();
@@ -138,7 +189,13 @@ public class ChessGame {
         return possibleMoves.toArray(new Move[0]);//allocate new array automatically.
     }
 
-    //TODO split in two
+    /**
+     * Checks to see if a move is valid to be taken from a perspective of movement of the piece or replacing a
+     * opposite color piece if the
+     * @param from
+     * @param to
+     * @return returns true if valid and false if it isn't
+     */
     private boolean isPossibleMoveForPiece(Tuple from, Tuple to){
         ChessPiece fromPiece = board.getTileFromTuple(from).getPiece();
         Move[] validMoves = fromPiece.moves();
@@ -146,8 +203,8 @@ public class ChessGame {
         int xMove = from.X() - to.X();
         int yMove = from.Y() - to.Y();
 
-        //Reverse values for black, such that lowering y-values are treated as moving forward
-        //this is only relevant in the case of pawns, but added for abstract movement assignment for pieces.
+        // Reverse values for black, such that lowering y-values are treated as moving forward
+        // only relevant in the case of pawns, but added for abstract movement assignment for pieces.
         if (currentPlayer == PieceColor.Black) {
             yMove = -yMove;
         }
@@ -156,14 +213,14 @@ public class ChessGame {
         if(!repeatableMoves){
             for (Move move : validMoves) {
                 if (move.x == xMove && move.y == yMove) {
-                    if (move.onTakeOnly){//if move is only legal on take (pawns)
+                    if (move.onTakeOnly){// if move is only legal on take (pawns)
                         Tile toTile = board.getTileFromTuple(to);
                         if (toTile.isEmpty()) break;
 
                         ChessPiece toPiece = toTile.getPiece();
-                        validMove = fromPiece.color() != toPiece.color();//if different color, valid move
+                        validMove = fromPiece.color() != toPiece.color();// if different color, valid move
                         break;
-                    //handling first move only for pawns
+                    // handling first move only for pawns
                     } else if (move.firstMoveOnly && (fromPiece.color() == PieceColor.White && from.Y() != 6
                                || fromPiece.color() == PieceColor.Black && from.Y() != 1)) {
                         break;
@@ -174,7 +231,7 @@ public class ChessGame {
                 }
             }
         } else {
-            for(int i = 0; i <= 8; i++){//Max number of repetitions
+            for(int i = 0; i <= 8; i++){// Max number of repetitions
                 for(Move move : validMoves) {
                     if (move.x * i == xMove && move.y * i == yMove) {
                         validMove = true;
@@ -186,6 +243,10 @@ public class ChessGame {
         return validMove;
     }
 
+    /**
+     * Returns the game status of the game.
+     * @return returns the game status
+     */
     public boolean isFinished(){
         return isFinished;
     }
