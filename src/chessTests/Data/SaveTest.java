49215f36a099fb0ae5a68411847d6f1397baa94b
Guardryan <Guard.ryan45@yahoo.com>
2017-03-11 15:46:45 -0500

    added Load tests

---
+++ src/chessTests/Data/SaveTest.java
@@ -0,0 +1,26 @@
+package Data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ryan on 3/11/2017.
+ */
+public class SaveTest {
+    @Test
+    public void clearAutoSave() throws Exception {
+
+    }
+
+    @Test
+    public void autoSave() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+}
