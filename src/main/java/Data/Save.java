3275db2eed92bb9b68b072e010b8ad128a74dc38
Brian Konzman <bkonzman@gmail.com>
2017-03-28 15:36:17 -0400

    program runs locally now

--- src/main/java/Data/Save.java
+++ src/main/java/Data/Save.java
@@ -54,8 +54,7 @@ public class Save {
      */
     public static void autoSave(ChessGame game) throws IOException {
         BufferedWriter autoSaveFile = new BufferedWriter(
-                new FileWriter(FILE_LOCATOR.baseFileLocation.substring(0,
-                        FILE_LOCATOR.baseFileLocation.length() - 14) + "/resources/main/AutoSave.txt",
+                new FileWriter(FILE_LOCATOR.toString() + "/resources/main/AutoSave.txt",
                         true));
         Tile[][] currentBoard = game.getBoard().getBoardArray();
         autoSaveFile.append(game.getCurrentPlayer().name());
@@ -98,10 +97,8 @@ public class Save {
      * @throws IOException
      */
     public static void save(String fromStr,String toStr) throws IOException {
-        File autoSaveFile = new File(FILE_LOCATOR.baseFileLocation.substring(0,
-                FILE_LOCATOR.baseFileLocation.length() - 14) + "/resources/main/" + fromStr + ".txt");
-        File saveFile = new File(FILE_LOCATOR.baseFileLocation.substring(0,
-                FILE_LOCATOR.baseFileLocation.length() - 14) + "/resources/main/" + toStr + ".txt");
+        File autoSaveFile = new File(FILE_LOCATOR.toString() + "/resources/main/" + fromStr + ".txt");
+        File saveFile = new File(FILE_LOCATOR.toString() + "/resources/main/" + toStr + ".txt");
 
         if(!saveFile.exists()){
             saveFile.createNewFile();
